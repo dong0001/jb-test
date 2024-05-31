60f92adf5307a8ee6b8d8abd9d44e52b708ff7a6
lizd <2580687674@qq.com>
2024-05-31 10:32:06 +0800

    no message

--- 1.js
+++ 1.js
@@ -1,13 +1,36 @@
 // ==UserScript==
-// @name         Hello World Script
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  A simple script to change the webpage title to "Hello, World!"
-// @author       You
-// @match        *://*/*
-// @grant        none
+// @name         test
+// @namespace    unrival
+// @version      3.6
+// @description  ▶▶▶上次更新：2024.05.29◀◀◀【💻可最小化💻】🆒支持超星视频、文档、答题、自定义正确率、掉线自动登录🤘取消视频文件加载，多开也不占用网速，放心追剧🍊自定义答题正确率，提高学习效率🍆每日功能测试，在发现问题前就解决问题，防清进度，无不良记录
+// @author       unrival
+// @run-at       document-end
+// @storageName  unrivalxxt
+// @match        *://*.chaoxing.com/*
+// @match        *://*.edu.cn/*
+// @match        *://*.nbdlib.cn/*
+// @match        *://*.hnsyu.net/*
+// @match        *://*.ac.cn/*
+// @icon         http://pan-yz.chaoxing.com/favicon.ico
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addValueChangeListener
+// @grant        GM_info
+// @connect      mooc1-1.chaoxing.com
+// @connect      mooc1.chaoxing.com
+// @connect      mooc1-2.chaoxing.com
+// @connect      passport2-api.chaoxing.com
+// @connect      14.29.190.187
+// @connect      cx.icodef.com
+// @license      GPL-3.0-or-later
+// @original-script https://scriptcat.org/script-show-page/878/
+// @original-author unrival
+// @original-license GPL-3.0-or-later
+// @downloadURL https://update.greasyfork.org/scripts/462748/%E3%80%90%E5%87%8F%E8%B4%9F%E7%A4%BE%E5%8C%BA%E3%80%91%E3%80%90%E8%B6%85%E6%98%9F%E5%AD%A6%E4%B9%A0%E9%80%9A%E5%B0%8F%E5%8A%A9%E6%89%8B%E3%80%91%E3%80%90%E5%AE%8C%E5%85%A8%E5%85%8D%E8%B4%B9%E3%80%91%E8%A7%86%E9%A2%91-%E7%AB%A0%E8%8A%82%E6%B5%8B%E8%AF%95%7C%E8%87%AA%E5%8A%A8%E6%8C%82%E6%9C%BA%7C%E9%98%B2%E6%B8%85%E8%BF%9B%E5%BA%A6%7C%E4%B8%8D%E5%8D%A0%E8%B5%84%E6%BA%90.user.js
+// @updateURL https://update.greasyfork.org/scripts/462748/%E3%80%90%E5%87%8F%E8%B4%9F%E7%A4%BE%E5%8C%BA%E3%80%91%E3%80%90%E8%B6%85%E6%98%9F%E5%AD%A6%E4%B9%A0%E9%80%9A%E5%B0%8F%E5%8A%A9%E6%89%8B%E3%80%91%E3%80%90%E5%AE%8C%E5%85%A8%E5%85%8D%E8%B4%B9%E3%80%91%E8%A7%86%E9%A2%91-%E7%AB%A0%E8%8A%82%E6%B5%8B%E8%AF%95%7C%E8%87%AA%E5%8A%A8%E6%8C%82%E6%9C%BA%7C%E9%98%B2%E6%B8%85%E8%BF%9B%E5%BA%A6%7C%E4%B8%8D%E5%8D%A0%E8%B5%84%E6%BA%90.meta.js
 // ==/UserScript==
-
 (function() {
     'use strict';
     document.title = 'Hello, World!';
